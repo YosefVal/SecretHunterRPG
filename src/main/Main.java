dded4a1c7051b0ddbcf67c4d3e1c41f379418cf5
YosefVal <yosefvalencia726@gmail.com>
2024-04-14 03:36:04 -0700

    Add classes

---
+++ src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		Game();
+	}
+	
+	public static void Game () {
+
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+		window.setTitle("Secret Hunter RPG");
+		
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		gamePanel.startGameThread();
+		
+	}
+}
